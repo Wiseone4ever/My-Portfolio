b992c5cb20648e78dcfa4b34c7b70de04d930be6
Vickyyyy98 <Vignesh.Parthasarathy@warwick.ac.uk>
2024-03-06 00:37:53 +0000

    R Script Updated

--- R/load_data.R
+++ R/load_data.R
@@ -3,5 +3,5 @@ library(RSQLite)
 
 customer_mock_data <- readr::read_csv("datasets/CUSTOMERS.csv")
 my_connection <- RSQLite::dbConnect(RSQLite::SQLite(),"database/ecommerce_databse.db")
-RSQLite::dbWriteTable(my_connection,"customers",customer_mock_data)
+RSQLite::dbWriteTable(my_connection,"customers",customer_mock_data,overwrite=TRUE)
 
